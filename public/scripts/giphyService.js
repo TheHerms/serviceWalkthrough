b3eaa9d3becc1cc468d55ecced86f30ae1459b26
Tyler Herman <tylerherman@Tylers-MacBook-Pro.local>
2017-02-03 14:23:02 -0600

    got gif to run

--- public/scripts/giphyService.js
+++ public/scripts/giphyService.js
@@ -5,11 +5,12 @@ var giphyUrl = "http://api.giphy.com/v1/gifs/";
 
 
   this.randomGif = function() {
-    $http({
+    return $http({
       type: 'GET',
       url: giphyUrl + 'random?api_key=' + publicAPIkey
     }).then(function(response){
-      console.log(response);
+      // console.log(response);
+      return response.data;
     }).catch(function(error){
       console.log(error);
     });
